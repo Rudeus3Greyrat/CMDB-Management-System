7e81d2b1908afa3ea57a82c542c9aebb1d0ffd23
Rudeus3Greyrat <3041084465@qq.com>
2021-03-03 17:43:56 +0800

    Initial

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Greyrat\Downloads\login-register-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
